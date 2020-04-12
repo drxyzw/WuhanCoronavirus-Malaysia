94a686b29eb102fc8e9eb3930c24c0a5559fb9b4
hirotoo20 <hirotoo20@gmail.com>
2020-04-12 22:02:37 +0900

    Adding using Active cases column instead of New cases for preliminary plot

--- .github/workflows/dataFromStarOnline.py
+++ .github/workflows/dataFromStarOnline.py
@@ -44,6 +44,21 @@ for i in range(len(tableBody)):
 	tableBody[i][0]  = dateStr
 tableBodyNp = np.flip(np.array(tableBody), axis = 0)
 table = np.append(tableHeaderNp, tableBodyNp, axis = 0)
+
+# save a total number file
+savetxt('../../data/totalRaw.csv', table, delimiter = ',', fmt = '%s')
+
+# Date, total cases, new cases, total death, total recovered --> Date, total cases, total death, total recovered, active cases
+table[0][2] = table[0][3]
+table[0][3] = table[0][4]
+table[0][4] = 'Active cases'
+
+for i in range(1, len(tableBody)):
+    table[i][2] = table[i][3]
+    table[i][3] = table[i][4]
+    table[i][4] = table[i][1] - table[i][3] - table[i][4]
+
+# save a total number file with active cases instead of new cases
 savetxt('../../data/dailyTotal.csv', table, delimiter = ',', fmt = '%s')
 
 print("Finished")
